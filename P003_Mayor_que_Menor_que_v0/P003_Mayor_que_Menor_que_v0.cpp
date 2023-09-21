fa7ec1cf8b7ed75c933214718cbb14019852a08c
SeusRollins <gil.pulido14@gmail.com>
2023-09-21 17:19:45 -0600

    000gil

--- P003_Mayor_que_Menor_que_v0/P003_Mayor_que_Menor_que_v0.cpp
+++ P003_Mayor_que_Menor_que_v0/P003_Mayor_que_Menor_que_v0.cpp
@@ -3,9 +3,31 @@
 
 #include <iostream>
 #include <math.h>
+#include <string>
+#include <locale>
 
 int main()
 {
-   
+    std::setlocale(LC_ALL, "es_MX.UTF-8");
+    float num1 = 0;
+    float num2 = 0;
+    std::cout << "dame tu primer numero por favor " << std::endl;
+    std::cin >> num1;
+    std::cout << "dame tu segundo numero por favor " << std::endl;
+    std::cin >> num2;
+    int mayor_edad = 7;
+    if (num1 > num2)
+    {
+        std::cout << num1 << " es mayor a " << num2 << std::endl;
+    }
+    else 
+    {
+        if (num1 < num2)
+        {
+            std::cout << num1 << " es menor a " << num2 << std::endl;
+        }
+    }
+    
+    
 }
 
