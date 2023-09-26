d68bdb75d976b3f4b1958e05733b1a7e87bf6928
SeusRollins <gil.pulido14@gmail.com>
2023-09-26 08:27:26 -0600

    00gil

--- P006_EControl_V0/P006_EControl_V0.cpp
+++ P006_EControl_V0/P006_EControl_V0.cpp
@@ -71,5 +71,28 @@ int main()
     default:
         std::cout << "Esa opción no la conozco\n";
     }
+    //Estructuras de Bucle
+   //El más peligroso! - While
+   //Mientras lo que esté entre parentesis este en Verdadero se ejecutara.
+    bool seguir = false;
+    while (seguir)
+    {
+        std::cout << "Perdiste una vida... Deseas continuar?. . .\n0.-No\n1.-Si\n";
+        std::cin >> seguir;
+    }
+    //El que se ejecutara si o si 1 vez - Do While
+    do
+    {
+        std::cout << "Entraste al do-while...0.-No\n1.-Si\n";
+        std::cin >> seguir;
+    } while (seguir);
+    //El que tiene mayor control - For
+    //1.- inicio; 2.- Limite; 3.- Tamaño de paso.
+    //i=i+1 o i++
+    for (int i = 0; i <= 100; i = i + 1)
+    {
+        std::cout << "El contador va en: " << i;
+        std::cout << std::endl;
+    }
 }
 
---
+++ P007 Decisiones (IF-ELSE-SWITCH CASE)_v0/P007 Decisiones (IF-ELSE-SWITCH CASE)_v0.cpp
@@ -0,0 +1,24 @@
+// P007 Decisiones (IF-ELSE-SWITCH CASE)_v0.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//GilPulido
+
+#include <iostream>
+
+int main()
+{
+    int royalrumble;
+    int eliminatoria;
+    int 
+
+    std::cout << "Hola bienvenido a la arena Mexico \n";
+    std::cout << "cual es el nombre de tu luchador \n";
+
+
+    std::cout << "donde finalmente tendremos un gandor de royal runle\n";
+    std::cout << "el cual enfrentara al campeon del toneo en el envemto principal de wrestlemania \n";
+    std::cout << "\n";
+    std::cout << "de cuantos luchadores serian tu royal rumble \n";
+    std::cout << "de 10 , 20 o 30 luchadores \n";
+    if
+
+}
+
