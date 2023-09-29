5730e4a70c00c8c42327d4b7657469e1cdb9a750
SeusRollins <gil.pulido14@gmail.com>
2023-09-28 22:32:35 -0600

    000gil

---
+++ P011_Condicionales_v0/P011_Condicionales_v0.cpp
@@ -0,0 +1,160 @@
+// P011_Condicionales_v0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//GilPulido
+
+#include <iostream>
+#include <locale>
+#include <string>
+
+int main()
+{
+    int opc =0;
+    std::setlocale(LC_ALL, "es_MX.UTF-8");
+    std::cout << "Adivina quien? \n a que compañia de lucha libre pertene\n el luchador\n";
+
+    std::cout << "Seth Rollins pertence a :\n";
+    std::cout << "1.- wwe, 2.-aew, 3.-aaa, 4.-njp\n";
+    std::cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        std::cout << "respuesta correcta el pertenece a wwe\n";
+        break;
+    case 2:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    case 3:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    case 4:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    default:
+        break;
+    }
+
+    std::cout << "kenny Omega pertence a :\n";
+    std::cout << "1.- wwe, 2.-aew, 3.-aaa, 4.-njp\n";
+    std::cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        std::cout << "respuesta incorrecta el pertenece a aew\n";
+        break;
+    case 2:
+        std::cout << "respuesta correcta el pertenece a aew\n";
+        break;
+    case 3:
+        std::cout << "respuesta incorrecta el pertenece a aew\n";
+        break;
+    case 4:
+        std::cout << "respuesta incorrecta el pertenece a aew\n";
+        break;
+    default:
+        break;
+    }
+
+    std::cout << "Cody Rhodes a :\n";
+    std::cout << "1.- wwe, 2.-aew, 3.-aaa, 4.-njp\n";
+    std::cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        std::cout << "respuesta correcta el pertenece a wwe\n";
+        break;
+    case 2:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    case 3:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    case 4:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    default:
+        break;
+    }
+
+    std::cout << "Chris Jericho pertence a :\n";
+    std::cout << "1.- wwe, 2.-aew, 3.-aaa, 4.-njp\n";
+    std::cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        std::cout << "respuesta incorrecta el pertenece a aew\n";
+        break;
+    case 2:
+        std::cout << "respuesta correcta el pertenece a aew\n";
+        break;
+    case 3:
+        std::cout << "respuesta incorrecta el pertenece a aew\n";
+        break;
+    case 4:
+        std::cout << "respuesta incorrecta el pertenece a aew\n";
+        break;
+    default:
+        break;
+    }
+    std::cout << "Psycho Clown pertence a :\n";
+    std::cout << "1.- wwe, 2.-aew, 3.-aaa, 4.-njp\n";
+    std::cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        std::cout << "respuesta incorrecta el pertenece a aaa\n";
+        break;
+    case 2:
+        std::cout << "respuesta incorrecta el pertenece a aaa\n";
+        break;
+    case 3:
+        std::cout << "respuesta correcta el pertenece a aaa\n";
+        break;
+    case 4:
+        std::cout << "respuesta incorrecta el pertenece a aaa\n";
+        break;
+    default:
+        break;
+    }
+
+    std::cout << "Will Ospreay pertence a :\n";
+    std::cout << "1.- wwe, 2.-aew, 3.-aaa, 4.-njp\n";
+    std::cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        std::cout << "respuesta incorrecta el pertenece a njp\n";
+        break;
+    case 2:
+        std::cout << "respuesta incorrecta el pertenece a njp\n";
+        break;
+    case 3:
+        std::cout << "respuesta incorrecta el pertenece a njp\n";
+        break;
+    case 4:
+        std::cout << "respuesta correcta el pertenece a njp\n";
+        break;
+    default:
+        break;
+    }
+
+    std::cout << "Roma Reings pertence a :\n";
+    std::cout << "1.- wwe, 2.-aew, 3.-aaa, 4.-njp\n";
+    std::cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        std::cout << "respuesta correcta el pertenece a wwe\n";
+        break;
+    case 2:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    case 3:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    case 4:
+        std::cout << "respuesta incorrecta el pertenece a wwe\n";
+        break;
+    default:
+        break;
+    }
+}
+
