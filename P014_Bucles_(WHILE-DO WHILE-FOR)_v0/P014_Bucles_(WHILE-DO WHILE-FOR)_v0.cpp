b2953c3e8278f57bcb68c46e6363b92ccad1de29
SeusRollins <gil.pulido14@gmail.com>
2023-10-13 20:52:40 -0600

    00gil

--- P014_Bucles_(WHILE-DO WHILE-FOR)_v0/P014_Bucles_(WHILE-DO WHILE-FOR)_v0.cpp
+++ P014_Bucles_(WHILE-DO WHILE-FOR)_v0/P014_Bucles_(WHILE-DO WHILE-FOR)_v0.cpp
@@ -1,21 +1,46 @@
-// P014_Bucles_(WHILE-DO WHILE-FOR)_v0.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// P014_Bucles_(WHILE-DO WHILE-FOR)_v0.cpp 
 //GilPulido
 
 #include <iostream>
-#include
+#include <ctime>
 
+void delay(int millisegundos)
+{
+    clock_t tiempo_de_carga = clock();
+    while (clock() < tiempo_de_carga + millisegundos);
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool repetir = true;
+    while (repetir)
+    {
+        //de 1000 a 0
+        for (int i = 1000; i >= 0; i--)
+        {
+            std::cout << "Contador" << i << std::endl;
+        }
+        std::cout << std::endl;
+        system("cls");
+        // contar lento 0 a 100
+        for (int i = 0; i <= 100; i++)
+        {
+            std::cout << "Contador" << i << std::endl;
+            delay(100);
+        }
+        std::cout << std::endl;
+        system("cls");
+        //10*10
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++) {
+                std::cout << "Contador" << i * j << std::endl;
+            }
+            std::cout << std::endl;
+            system("cls");
+        }
+        std::cout << "Deseas repetir el contador? 0.- NO 1.- SI: ";
+        std::cin >> repetir;
+        system("cls");
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
