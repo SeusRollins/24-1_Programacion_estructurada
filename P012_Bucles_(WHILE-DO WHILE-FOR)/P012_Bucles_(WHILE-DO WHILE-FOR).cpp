f1474f767e8dbc0ec1202c176e8972f45b472871
SeusRollins <gil.pulido14@gmail.com>
2023-10-13 23:33:47 -0600

    00gil
    codigo juego 3 vidas

--- P012_Bucles_(WHILE-DO WHILE-FOR)/P012_Bucles_(WHILE-DO WHILE-FOR).cpp
+++ P012_Bucles_(WHILE-DO WHILE-FOR)/P012_Bucles_(WHILE-DO WHILE-FOR).cpp
@@ -1,20 +1,124 @@
-// P012_Bucles_(WHILE-DO WHILE-FOR).cpp : This file contains the 'main' function. Program execution begins and ends there.
+// P012_Bucles_(WHILE-DO WHILE-FOR).cpp 
 //GilPulido
 
 #include <iostream>
+#include <locale>
+#include <string>
+#include <Windows.h>
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "es_MX.UTF-8");
+    std::string respuesta;
+    int i = 100;
+    bool repetir = false;
+    do
+    {
+        std::cout << "Hola Bienvenido al infierno de los Dioses, en este juego tienes 3 vidas \n" <<
+            "Donde tus son tu salvacion o tu condena dependen de ti\n"
+            << "Quieres jugar? \n" << "s = Si, n = NO \n";
+        std::cin >> respuesta;
+        system("cls");
+        if (respuesta == "s")
+        {
+            std::cout << "Vaya veo que eres valienten\n";
+            std::cout << "Un perro infernal esta justo al frente de ti\n";
+            std::cout << "Quieres enfrentarte a el : s/n \n";
+            std::cin >> respuesta;
+            system("cls");
+            for (i = 100; i >= 0; i--)
+            {
+                std::cout << "contador" << i << std::endl;
+                system("cls");
+            }
+            if (respuesta == "s")
+            {
+                std::cout << "La valentia es algo que se respetan entre dioses  \n" <<
+                    "Pero como no eres un dios pierdes una vida\n" << 
+                    "ahora tienes 2 vidas \n";
+                std::cout << "ahora estas de frente de un angel maldito\n";
+                std::cout << "te enfrentas a el  s/n \n";
+                std::cin >> respuesta;
+                system("cls");
+                for (i = 100; i >= 0; i--)
+                {
+                    std::cout <<"contador " << i << std::endl;
+                    system("cls");
+                }
+                if (respuesta == "s")
+                {
+                    std::cout << "Vaya veo que eres muy valiente \n";
+                    std::cout << "pero el angel infernal fue mas fuerte te mato\n";
+                    std::cout << "ahora te queda 1 vidad\n";
+                    std::cout << "tu opciones y posibilidades son pocas\n";
+                    std::cout << "Deseas continuar, s/n \n";
+                    std::cin >> respuesta;
+                    system("cls");
+                    for (i = 100; i >= 0; i--)
+                    {
+                        std::cout << "contador " << i << std::endl;
+                        system("cls");
+                    }
+                    if (respuesta == "s")
+                    {
+                        std::cout << "tu valentia es increible\n";
+                        std::cout << "lamentblemente  en este juego tu unico destino es morir\n";
+                        std::cout << "pero tu mueres como un dios , como un heroe, como una leyenda\n";
+                        std::cout << "has perdido pero tienes mi respeto\n";
+                        std::cout << "pierdes la vida ante un demonio\n";
+                        std::cout << "Deseas volver a jugar? s/n \n";
+                        std::cin >> respuesta;
+
+
+                        for (i = 100; i >= 0; i--)
+                        {
+                            std::cout << "contador " << i << std::endl;
+                            system("cls");
+                            std::cout << "GameOver\n";
+                        }
+                    }
+                    else
+                    {
+                        std::cout << "fuiste muy valiente pero al final te falto valor\n";
+                        std::cout << "has sido asesinado por un demonio\n";
+                     
+                    }
+                    for (i = 100; i >= 0; i--)
+                    {
+                        std::cout << "contador " << i << std::endl;
+                        system("cls");
+                        std::cout << "GameOver\n";
+                    }
+                }
+                else
+                {
+                    std::cout << "Al principio fuiste valiente pero la covardia no es aceptada \n";
+                    std::cout << "entre dioses y demenoios.\n";
+                    std::cout << "asi que pierdes tus 2 vidas restantes\n";
+                    std::cout << "Game Over\n";
+                    system("cls");
+
+                }
+            }
+            else
+            {
+                std::cout << "puedes pensar que has elegido saviamente pero te equivocaste \n" << "Perdiste todas tus vidas restantes \n";
+                std::cout << "Game Over\n";
+                system("cls");
+            }
+        }
+        else
+        {
+            std::cout << "pierdes tus 3 vidas por cobarde \n";
+            std::cout << "El infierno de los dioses no es para todos\n";
+            std::cout << "Game Over\n";
+            system("cls");
+        }
+
+    } while (true);
+    system("cls");
+    return 0;
+
+   
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
