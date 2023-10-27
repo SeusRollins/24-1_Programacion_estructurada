9729516fb35712f6ade34448a97e8f8e87a0655d
SeusRollins <gil.pulido14@gmail.com>
2023-10-27 10:27:50 -0600

    01

--- P014_Bucles_(WHILE-DO WHILE-FOR)_v0/P014_Bucles_(WHILE-DO WHILE-FOR)_v0.cpp
+++ P014_Bucles_(WHILE-DO WHILE-FOR)_v0/P014_Bucles_(WHILE-DO WHILE-FOR)_v0.cpp
@@ -13,7 +13,7 @@ void delay(int millisegundos)
 int main()
 {
     bool repetir = true;
-    int opt;
+    int opt,i,contador;
     while (repetir)
     {
         std::cout << "1.- de 1000 a 0, 2.- 0 a 100 lento, 3.- 10x10\n";
@@ -21,23 +21,21 @@ int main()
         std::cin >> opt;
         switch (opt)
         {
-        default:
-            break;
         case 1:
             // de 1000 a 0
             // for
-            for (int i = 1000; i >= 0; i--)
+            for (i = 1000; i >= 0; i--)
             {
                 std::cout << "Contador " << i << std::endl;
             }
             // while
-            int i = 0;
+            i = 0;
             while (i <= 1000) {
                 std::cout << "Contador " << i << " ";
                 i++;
             }
             // do-while
-            int contador = 0;
+            contador = 0;
             do {
                 std::cout << contador << " ";
                 contador++;
@@ -47,7 +45,7 @@ int main()
 
         case 2:
             // contar lentamente de 0 a 100
-            for (int i = 0; i <= 100; i++)
+            for ( i = 0; i <= 100; i++)
             {
                 std::cout << "Contador " << i << std::endl;
                 delay(200);
@@ -57,7 +55,7 @@ int main()
 
         case 3:
             // 10x10
-            for (int i = 1; i <= 100; i++)
+            for ( i = 1; i <= 100; i++)
             {
                 std::cout << i << " ";
                 if (i % 10 == 0) {
@@ -65,10 +63,12 @@ int main()
                 }
             }
             break;
+        default:
+            std::cout << "No conozco esa opción\n";
+            break;
         }
         std::cout << "¿Deseas repetir el contador? 0.- NO 1.- SI: ";
         std::cin >> repetir;
     }
 
-    return 0;
 }
