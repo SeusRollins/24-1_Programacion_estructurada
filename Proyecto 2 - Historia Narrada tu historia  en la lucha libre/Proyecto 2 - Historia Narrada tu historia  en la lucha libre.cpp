8eacc7ada5b83876d4701311dcdca7e35db9799f
SeusRollins <gil.pulido14@gmail.com>
2023-11-28 17:53:58 -0600

    001

--- Proyecto 2 - Historia Narrada tu historia  en la lucha libre/Proyecto 2 - Historia Narrada tu historia  en la lucha libre.cpp
+++ Proyecto 2 - Historia Narrada tu historia  en la lucha libre/Proyecto 2 - Historia Narrada tu historia  en la lucha libre.cpp
@@ -40,16 +40,39 @@ int main()
             case 1:
             {
                 std::cout << "Bienvenido a Raw " << nobreluchador << " aqui comienza tu historia\n";
+                std::cout << "para tu debut tienes dos posibles luhas\n";
+                std::cout << "1.-luchar contra el campeon , 2.- luchar en la celda infernal \n";
+                std::cin >> decision;
+                if (decision = true)
+                {
+                    std::cout << "el campeon es  alguien dificil de vencer por eso es no le durate nada pero te ganaste el respeto del campeon\n";
+                    std::cout << "el campeon  te ofece su mano en señal de resto \n";
+                    std::cout << "1.- le das la mano , 2.- lo golpeas\n";
+                    std::cin >> decision;
+                    if (decision = true);
+                    {
+                        std::cout << "el publico explota en abucheos contra ti\n";
+                        std::cout << "ahora tines la atencion de todos en la erena \n";
+                        std::cout << "que vas hacer anete esta reacion?\n";
+                        std::cout << "1.-tomar el campeonato,2.-pisar el campeonato y huir\n";
+                        std::cin >> decision;
+                        if()
+                    } 
+                }
+                else
+                {
+
+                }
                 break;
             }
             case 2:
             {
-                std::cout << "Bienvenido a Raw " << nobreluchador << " aqui comienza tu historia\n";
+                std::cout << "Bienvenido a SmackDonw " << nobreluchador << " aqui comienza tu historia\n";
                 break;
             }
             case 3:
             {
-                std::cout << "Bienvenido a Raw " << nobreluchador << " aqui comienza tu historia\n";
+                std::cout << "Bienvenido a Nxt " << nobreluchador << " aqui comienza tu historia\n";
                 break;
             }
             default:
