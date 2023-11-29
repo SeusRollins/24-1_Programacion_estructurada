b085b93369b7041f88f7eb91bf18248bf21c6340
SeusRollins <gil.pulido14@gmail.com>
2023-11-29 00:58:53 -0600

    gil

---
+++ P028_Tutorial_1_v0/P028_Tutorial_1_v0.cpp
@@ -0,0 +1,27 @@
+// P028_Tutorial_1_v0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//GilPulido
+#include <MathLibrary.h>
+
+namespace MathLibrary
+{
+    double Arithmetic::Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    double Arithmetic::Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    double Arithmetic::Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    double Arithmetic::Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
+
