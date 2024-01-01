e2ee9d89ea4a5b9939e0a3a2f49b19fe12aa17fa
SeusRollins <gil.pulido14@gmail.com>
2023-12-31 18:00:00 -0600

    proyecto

---
+++ P020_Adivina_quien!/P020_Adivina_quien!.cpp
@@ -0,0 +1,12 @@
+// P020_Adivina_quien!.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hola soy Mr. WWE el adivinador\n";
+    std::cout << "voy a adivinar tu luchardor favorito\n";
+    std::cout << "con un par de preguntas de si o no\n";
+}
+
